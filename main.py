68784ff1905454976a2e0ba1d04b364c6544dcc2
ra559 <arobertadrian@gmail.com>
2026-04-14 12:13:14 -0400

    working on ch1

---
+++ main.py
@@ -0,0 +1,5 @@
+print("=" * 80)
+print("No main.py was found so one was created!")
+print("Add your own code to this file: /media/rap/storage2/projects/newgameplus/main.py")
+print("=" * 80)
+input("Key enter to close.")
